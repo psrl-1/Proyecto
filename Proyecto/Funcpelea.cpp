1aeafa97cdb95ed0595622efba691ba87471615b
Paola Rodriguez <00041826@uca.edu.sv>
2026-06-25 15:00:38 -0600

    Sistema de peleas

--- Proyecto/Funcpelea.cpp
+++ Proyecto/Funcpelea.cpp
@@ -35,11 +35,83 @@ int main()
             }
         }
 
-        if (juego ==  true)
+        if (juego ==  true) // Verificacion de el estado del juego en caso de que la variable juego se marcara false.
         {
+            // Pokemon 1 vs pokemon 2
             cout << j1 << ": Pokemon " << poke1[actualj1] << " (Vida: " << vidaj1[actualj1] << ")" << endl;
             cout << "VS" << endl;
             cout << j2 <<": Pokemon " << poke2[actualj2] << " (Vida: " << vidaj2[actualj2] << ")" << endl;
+
+            // Pelea separada y verificada por turnos.
+            if (turno == 1)
+            {
+                cout << "Menu: \n"<< "1. Usar ataque 1 \n" << "2. Usar ataque 2\n" << "3. Cambiar pokemon\n" << "4. Rendirse \n";
+                cout << "Opcion: ";
+                cin >> opcion;
+
+                switch (opcion)
+                {
+                    case 1:
+                    cout << "Usaste el ataque 1" << endl;
+                    vidaJ2[actualJ2] = vidaJ2[actualJ2] - dano[poke1[actualJ1]-1][0];
+                    cout << "Vida del rival: " << vidaJ2[actualJ2];
+                    turno = 2;
+                    break;
+                    case 2:
+                    cout << "Lanzaste el ataque 2" << endl;
+                    vidaJ2[actualJ2] = vidaJ2[actualJ2] - dano[poke1[actualJ1]-1][1];
+                    cout << "Vida del rival: " << vidaJ2[actualJ2];
+                    turno = 2;
+                    break;
+                    case 3: 
+                    cout << "Pokémons disponibles: "<< endl;
+                    // menu del cambio de pokemon
+                    case 4: 
+                    cout << "Te has rendido " << endl;
+                    ganador = 2;
+                    juego == false;
+                    break;
+                    default:
+                    cout << "Opcion invalida. Pierdes turno." << endl;
+                    turno = 2;
+                    break;
+                }
+
+            }
+            else 
+            {
+                                cout << "Menu: \n"<< "1. Usar ataque 1 \n" << "2. Usar ataque 2\n" << "3. Cambiar pokemon\n" << "4. Rendirse \n";
+                cout << "Opcion: ";
+                cin >> opcion;
+
+                switch (opcion)
+                {
+                    case 1:
+                    cout << "Usaste el ataque 1" << endl;
+                    vidaJ1[actualJ1] = vidaJ1[actualJ1] - dano[poke2[actualJ2]-1][0];
+                    cout << "Vida del rival: " << vidaJ1[actualJ1];
+                    turno = 1;
+                    break;
+                    case 2:
+                    cout << "Lanzaste el ataque 2" << endl;
+                    vidaJ1[actualJ1] = vidaJ1[actualJ1] - dano[poke2[actualJ2]-1][1];
+                    cout << "Vida del rival: " << vidaJ2[actualJ2];
+                    turno = 1;
+                    break;
+                    case 3: 
+                    cout << "Pokémons disponibles: "<< endl;
+                    // menu del cambio de pokemon
+                    case 4: 
+                    cout << "Te has rendido " << endl;
+                    ganador = 1;
+                    juego == false;
+                    break;
+                    default:
+                    cout << "Opcion invalida. Pierdes turno." << endl;
+                    turno = 1;
+                    break;
+            }
+
         }
     }
 }
