74baee0d6dfeeb3600c9cdb7a47151fd8975be40
Paola Rodriguez <00041826@uca.edu.sv>
2026-06-25 14:12:18 -0600

    Variables base

--- Anteproyecto/pokemon.cpp
+++ Anteproyecto/pokemon.cpp
@@ -29,8 +29,7 @@ int main()
     int Mewtwo[5] = {11, 106, 110, 90, 130};
     int Chansey[5] = {12, 250, 5, 5, 50};
 
-    for (int i = 1; i <= 3; i++)
-    {
+
         // Primera, segunda y terccera desiccion depojemosn
         cout << "Elige la clase para el pokemon " << i << endl;
         cout << "Fuego: 1\n";
@@ -39,89 +38,8 @@ int main()
         cout << "Clase: ";
         cin >> clase;
 
-        // clases
-        switch (clase)
-        {
-        case 1:
-            cout << "Elige un pokemon.\n";
-            cout << "Charmander: 1\n";
-            cout << "Arcanine: 2\n";
-            cout << "Torchic: 3\n";
-            cin >> poke;
 
-            switch (poke)
-            {
-            case 1:
-                j1poke1[5] = Charmander[5];
-                cout << "Has escogido a Charmander!\n";
-                break;
-            case 2:
-                j1poke2[5] = Arcanine[5];
-                cout << "Has escogido a Arcanine!\n";
-                break;
-            case 3:
-                j1poke3[5] = Torchic[5];
-                cout << "Has escogido a Torchic!\n";
-                break;
-            default:
-                cout << "No escogiste a tiempo!\n";
-                break;
-            }
-            break;
-        case 2:
-            cout << "Elige un pokemon.\n";
-            cout << "Bulbasaur: 1\n";
-            cout << "Chikorita: 2\n";
-            cout << "Floragato: 3\n";
-            cin >> poke;
-
-            switch (poke)
-            {
-            case 1:
-                j1poke1[5] = Bulbasaur[5];
-                cout << "Has escogido a Bulbasaur!\n";
-                break;
-            case 2:
-                j1poke2[5] = Chikorita[5];
-                cout << "Has escogido a Chikorita!\n";
-                break;
-            case 3:
-                j1poke3[5] = Floragato[5];
-                cout << "Has escogido a Floragato!\n";
-                break;
-            default:
-                cout << "No elegiste a tiempo!\n";
-                break;
-            }
-            break;
-        case 3:
-            cout << "Elige un pokemon.\n";
-            cout << "Squirtle: 1\n";
-            cout << "Wooper: 2\n";
-            cout << "Finizen: 3\n";
-            cin >> poke;
-
-            switch (poke)
-            {
-            case 1:
-                j1poke1[5] = Squirtle[5];
-                cout << "Has escogido a Squirtle!\n";
-                break;
-            case 2:
-                j1poke2[5] = Wooper[5];
-                cout << "Has escogido a Wooper!\n";
-                break;
-            case 3:
-                j1poke3[5] = Finizen[5];
-                cout << "Has escogido a Finizen!\n";
-                break;
-            default:
-                cout << "No has escogido a tiempo!\n";
-                break;
-            }
-            break;
-        }
-    }
+    
 
     return 0;
 }
